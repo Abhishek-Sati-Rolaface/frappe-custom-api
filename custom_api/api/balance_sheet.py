3419472fec3b0126b02c8417d3b6aafc6ea172c2
Abhishek Sati <abhishek.sati@rolaface.com>
2026-03-12 16:59:09 +0530

    added filter_based_on filter

--- custom_api/api/balance_sheet.py
+++ custom_api/api/balance_sheet.py
@@ -56,11 +56,13 @@ def _build_tree(raw_data, period_keys):
 @frappe.whitelist(allow_guest=False, methods=["GET"])
 def get_balance_sheet():
     company = frappe.defaults.get_user_default("Company")
-    from_date = frappe.request.args.get("from_date")
-    to_date = frappe.request.args.get("to_date")
+    current_year = frappe.utils.now_datetime().year
+    from_date = frappe.request.args.get("from_date", None)
+    to_date = frappe.request.args.get("to_date", None)
     periodicity = frappe.request.args.get("periodicity", "Yearly")
-    from_fiscal_year = frappe.request.args.get("from_fiscal_year", None)
-    to_fiscal_year = frappe.request.args.get("to_fiscal_year", None)
+    from_fiscal_year = frappe.request.args.get("from_fiscal_year", current_year)
+    to_fiscal_year = frappe.request.args.get("to_fiscal_year", current_year)
+    filter_based_on = frappe.request.args.get("filter_based_on", "Fiscal Year")
 
     filters = frappe._dict({
         "company": company,
@@ -68,9 +70,9 @@ def get_balance_sheet():
         "to_fiscal_year": to_fiscal_year,
         "period_start_date": from_date,
         "period_end_date": to_date,
-        "filter_based_on": "Fiscal Year",
+        "filter_based_on": filter_based_on,
         "periodicity": periodicity,
-        "accumulated_values": 0,
+        "selected_view": "Report"
     })
 
     columns, data, message, chart, report_summary, primitive_summary = execute(filters)
--- custom_api/api/profit_loss.py
+++ custom_api/api/profit_loss.py
@@ -56,11 +56,13 @@ def _build_tree(raw_data, period_keys):
 @frappe.whitelist(allow_guest=False, methods=["GET"])
 def get_profit_and_loss():
     company = frappe.defaults.get_user_default("Company")
+    current_year = frappe.utils.now_datetime().year
     from_date = frappe.request.args.get("from_date", None)
     to_date = frappe.request.args.get("to_date", None)
     periodicity = frappe.request.args.get("periodicity", "Yearly")
-    from_fiscal_year = frappe.request.args.get("from_fiscal_year", None)
-    to_fiscal_year = frappe.request.args.get("to_fiscal_year", None)
+    from_fiscal_year = frappe.request.args.get("from_fiscal_year", current_year)
+    to_fiscal_year = frappe.request.args.get("to_fiscal_year", current_year)
+    filter_based_on = frappe.request.args.get("filter_based_on", "Fiscal Year")
 
     filters = frappe._dict({
         "company": company,
@@ -68,7 +70,7 @@ def get_profit_and_loss():
         "to_fiscal_year": to_fiscal_year,
         "period_start_date": from_date,
         "period_end_date": to_date,
-        "filter_based_on": "Fiscal Year",
+        "filter_based_on": filter_based_on,
         "periodicity": periodicity,
         "accumulated_values": 0,
     })
