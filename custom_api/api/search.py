0c85fcc4c901d93c3ea12270d3be7a7a69bfe835
Akhilesh Rawat <akhileshrawat.asr@gmail.com>
2026-03-17 17:02:47 +0530

    Add lookup API for cost centers, customers, suppliers, company payable and receivable accounts

--- custom_api/api/search.py
+++ custom_api/api/search.py
@@ -2,6 +2,7 @@ from frappe.desk.search import build_for_autosuggest, search_widget
 import frappe
 from custom_api.utils.response import send_response
 
+
 @frappe.whitelist(allow_guest=False, methods=["GET"])
 def get_bank_accounts():
     try:
@@ -20,32 +21,25 @@ def get_bank_accounts():
         return send_response(
             status="success",
             message="Bank Accounts fetched successfully.",
-            data={
-                "data": response
-            },
+            data={"data": response},
             status_code=200,
             http_status=200,
         )
     except Exception as e:
         frappe.log_error(frappe.get_traceback(), "Make Payment API Error")
         return send_response(
-            status="fail",
-            message=str(e),
-            data=None,
-            status_code=500,
-            http_status=500
-            )
+            status="fail", message=str(e), data=None, status_code=500, http_status=500
+        )
+
 
 @frappe.whitelist(allow_guest=False, methods=["GET"])
 def get_company_accounts():
     try:
         txt = frappe.request.args.get("search", "")
         company = frappe.defaults.get_user_default("Company")
-        filters = frappe._dict({
-            "company": company,
-            "account_type": "Bank",
-            "is_group":0
-        })
+        filters = frappe._dict(
+            {"company": company, "account_type": "Bank", "is_group": 0}
+        )
         results = search_widget(
             "Account",
             txt.strip(),
@@ -60,18 +54,181 @@ def get_company_accounts():
         return send_response(
             status="success",
             message="Company Accounts fetched successfully.",
-            data={
-                "data": response
-            },
+            data={"data": response},
             status_code=200,
             http_status=200,
         )
     except Exception as e:
         frappe.log_error(frappe.get_traceback(), "Make Payment API Error")
         return send_response(
-            status="fail",
-            message=str(e),
-            data=None,
-            status_code=500,
-            http_status=500
-            )
+            status="fail", message=str(e), data=None, status_code=500, http_status=500
+        )
+
+
+@frappe.whitelist(allow_guest=False, methods=["GET"])
+def get_payable_accounts():
+    try:
+        txt = frappe.request.args.get("search", "")
+        company = frappe.defaults.get_user_default("Company")
+        filters = frappe._dict(
+            {"company": company, "account_type": "Payable", "is_group": 0}
+        )
+        results = search_widget(
+            "Account",
+            txt.strip(),
+            None,
+            searchfield=None,
+            page_length=10,
+            filters=filters,
+            reference_doctype="Account",
+            ignore_user_permissions=0,
+        )
+        response = build_for_autosuggest(results, doctype="Account")
+        return send_response(
+            status="success",
+            message="Payable Accounts fetched successfully.",
+            data={"data": response},
+            status_code=200,
+            http_status=200,
+        )
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), "Get Payable Accounts API Error")
+        return send_response(
+            status="fail", message=str(e), data=None, status_code=500, http_status=500
+        )
+
+
+@frappe.whitelist(allow_guest=False, methods=["GET"])
+def get_receivable_accounts():
+    try:
+        txt = frappe.request.args.get("search", "")
+        company = frappe.defaults.get_user_default("Company")
+        filters = frappe._dict(
+            {"company": company, "account_type": "Receivable", "is_group": 0}
+        )
+        results = search_widget(
+            "Account",
+            txt.strip(),
+            None,
+            searchfield=None,
+            page_length=10,
+            filters=filters,
+            reference_doctype="Account",
+            ignore_user_permissions=0,
+        )
+        response = build_for_autosuggest(results, doctype="Account")
+        return send_response(
+            status="success",
+            message="Receivable Accounts fetched successfully.",
+            data={"data": response},
+            status_code=200,
+            http_status=200,
+        )
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), "Get Receivable Accounts API Error")
+        return send_response(
+            status="fail", message=str(e), data=None, status_code=500, http_status=500
+        )
+
+
+@frappe.whitelist(allow_guest=False, methods=["GET"])
+def get_cost_centers():
+    try:
+        txt = frappe.request.args.get("search", "")
+        company = frappe.defaults.get_user_default("Company")
+
+        filters = frappe._dict({"company": company})
+
+        results = search_widget(
+            "Cost Center",
+            txt.strip(),
+            None,
+            searchfield=None,
+            page_length=10,
+            filters=filters,
+            reference_doctype="Cost Center",
+            ignore_user_permissions=0,
+        )
+
+        response = build_for_autosuggest(results, doctype="Cost Center")
+
+        return send_response(
+            status="success",
+            message="Cost Centers fetched successfully.",
+            data={"data": response},
+            status_code=200,
+            http_status=200,
+        )
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), "Get Cost Centers API Error")
+        return send_response(
+            status="fail", message=str(e), data=None, status_code=500, http_status=500
+        )
+
+
+@frappe.whitelist(allow_guest=False, methods=["GET"])
+def get_customers():
+    try:
+        txt = frappe.request.args.get("search", "")
+
+        filters = frappe._dict({})
+
+        results = search_widget(
+            "Customer",
+            txt.strip(),
+            None,
+            searchfield=None,
+            page_length=10,
+            filters=filters,
+            reference_doctype="Customer",
+            ignore_user_permissions=0,
+        )
+
+        response = build_for_autosuggest(results, doctype="Customer")
+
+        return send_response(
+            status="success",
+            message="Customers fetched successfully.",
+            data={"data": response},
+            status_code=200,
+            http_status=200,
+        )
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), "Get Customers API Error")
+        return send_response(
+            status="fail", message=str(e), data=None, status_code=500, http_status=500
+        )
+
+
+@frappe.whitelist(allow_guest=False, methods=["GET"])
+def get_suppliers():
+    try:
+        txt = frappe.request.args.get("search", "")
+
+        filters = frappe._dict({})
+
+        results = search_widget(
+            "Supplier",
+            txt.strip(),
+            None,
+            searchfield=None,
+            page_length=10,
+            filters=filters,
+            reference_doctype="Supplier",
+            ignore_user_permissions=0,
+        )
+
+        response = build_for_autosuggest(results, doctype="Supplier")
+
+        return send_response(
+            status="success",
+            message="Suppliers fetched successfully.",
+            data={"data": response},
+            status_code=200,
+            http_status=200,
+        )
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), "Get Suppliers API Error")
+        return send_response(
+            status="fail", message=str(e), data=None, status_code=500, http_status=500
+        )
