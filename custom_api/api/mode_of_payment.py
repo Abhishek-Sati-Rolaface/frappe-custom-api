29481efe2a14b59af8f5b32a7d8848367ae9355f
Abhishek Sati <abhishek.sati@rolaface.com>
2026-03-19 21:00:29 +0530

    created get API

--- custom_api/api/mode_of_payment.py
+++ custom_api/api/mode_of_payment.py
@@ -1,5 +1,6 @@
 from erpnext.zra_client.generic_api import send_response
 import frappe
+from frappe.utils import ceil
 
 @frappe.whitelist(allow_guest=False, methods=["POST"])
 def create():
@@ -58,3 +59,60 @@ def create():
         return send_response(
             status="fail", message="Something went wrong, Please try again later", data=None, status_code=500, http_status=500
         )
+
+@frappe.whitelist(allow_guest=False, methods=["GET"])
+def get():
+    company      = frappe.request.args.get("company") or frappe.defaults.get_user_default("Company")
+    payment_type = frappe.request.args.get("type")
+    enabled      = frappe.request.args.get("enabled")
+    name         = frappe.request.args.get("name")
+    search       = frappe.request.args.get("search")
+    page         = int(frappe.request.args.get("page", 1))
+    page_size    = int(frappe.request.args.get("page_size", 2))
+
+    filters = []
+    if payment_type:
+        filters.append(["type", "=", payment_type])
+    if enabled is not None:
+        filters.append(["enabled", "=", int(enabled)])
+    if name:
+        filters.append(["name", "=", name])
+    if search:
+        filters.append(["mode_of_payment", "like", f"%{search}%"])
+
+    total = frappe.db.count("Mode of Payment", filters=filters)
+    mop_list = frappe.db.get_all(
+        "Mode of Payment",
+        filters=filters,
+        fields=["name", "mode_of_payment as modeOfPayment", "type", "enabled"],
+        order_by="creation desc",
+        limit=page_size,
+        limit_start=(page - 1) * page_size,
+    )
+
+    for mop in mop_list:
+        account = frappe.db.get_value(
+            "Mode of Payment Account",
+            {"parent": mop["name"], "company": company},
+            ["default_account"],
+            as_dict=True
+        )
+        mop["defaultAccount"] = account.get("default_account") if account else None
+
+    return send_response(
+        status="success",
+        message="Mode of Payments fetched successfully.",
+        data={
+            "modeOfPayments": mop_list,
+            "pagination": {
+                "total": total,
+                "page": page,
+                "page_size": page_size,
+                "total_pages": ceil(total / page_size),
+                "has_next": page * page_size < total,
+                "has_prev": page > 1,
+            }
+        },
+        status_code=200,
+        http_status=200,
+    )
