a54c6b445d35c74ca333ef82a994cbe7c607291b
mac <mac@macs-MacBook-Air.local>
2026-05-27 16:41:11 +0530

    Fix the rate issue for barcode scan

--- custom_api/utils/barcode_utils.py
+++ custom_api/utils/barcode_utils.py
@@ -1,78 +1,9 @@
-# import frappe
-# @frappe.whitelist()
-# def get_item_from_batch_barcode(barcode):
-#     """Batch ke custom_barcode se item aur batch fetch karta hai"""
-    
-#     batch = frappe.db.get_value(
-#         "Batch",
-#         {"custom_barcode": str(barcode)},
-#         ["name", "item", "batch_id", "expiry_date"],
-#         as_dict=True
-#     )
-
-#     if not batch:
-#         return None
-
-#     # Item ki details fetch karo
-#     item = frappe.db.get_value(
-#         "Item",
-#         batch.item,
-#         ["item_name", "stock_uom", "description"],
-#         as_dict=True
-#     )
-
-#     return {
-#         "item_code": batch.item,
-#         "item_name": item.item_name,
-#         "batch_no": batch.batch_id,
-#         "uom": item.stock_uom,
-#         "expiry_date": batch.expiry_date,
-#         "barcode": barcode
-#     }
-
-
-# import frappe
-
-# @frappe.whitelist()
-# def get_item_from_batch_barcode(barcode):
-#     """Batch ke custom_barcode se item aur batch fetch karta hai"""
-
-#     batch = frappe.db.get_value(
-#         "Batch",
-#         {"custom_barcode": str(barcode)},
-#         ["name", "item", "batch_id", "expiry_date"],
-#         as_dict=True
-#     )
-
-#     if not batch:
-#         return None
-
-#     item = frappe.db.get_value(
-#         "Item",
-#         batch.item,
-#         ["item_name", "stock_uom"],
-#         as_dict=True
-#     )
-
-#     if not item:
-#         return None
-
-#     return {
-#         "item_code": batch.item,
-#         "item_name": item.item_name,
-#         "batch_no": batch.batch_id,
-#         "uom": item.stock_uom,
-#         "expiry_date": str(batch.expiry_date) if batch.expiry_date else None,
-#         "barcode": barcode
-#     }
-
-
 import frappe
 
 
 @frappe.whitelist()
 def get_item_from_batch_barcode(barcode):
-    """Batch ke custom_barcode se item aur batch fetch karta hai"""
+    """Batch Barcode from item details """
     try:
         batch = frappe.db.get_value(
             "Batch",
@@ -95,11 +26,28 @@ def get_item_from_batch_barcode(barcode):
             frappe.log_error(f"Item {batch.item} not found", "Barcode Scan")
             return None
 
+        # Default selling price list fetch karo
+        selling_price_list = frappe.db.get_single_value(
+            "Selling Settings", "selling_price_list"
+        ) or "Standard Selling"
+
+        # Rate fetch karo Item Price se
+        rate = frappe.db.get_value(
+            "Item Price",
+            {
+                "item_code": batch.item,
+                "selling": 1,
+                "price_list": selling_price_list
+            },
+            "price_list_rate"
+        ) or 0
+
         return {
             "item_code": batch.item,
             "item_name": item.item_name,
             "batch_no": batch.batch_id,
             "uom": item.stock_uom,
+            "rate": rate,
             "expiry_date": str(batch.expiry_date) if batch.expiry_date else None,
             "barcode": barcode
         }
