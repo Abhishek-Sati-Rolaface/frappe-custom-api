4549ba4b3e541ac2c2ee4daae642c6444e80773f
mac <mac@macs-MacBook-Air.local>
2026-06-15 11:01:03 +0530

    Check the csrf issue

--- custom_api/custom_api/whatsapp.py
+++ custom_api/custom_api/whatsapp.py
@@ -4,9 +4,9 @@ import requests
 @frappe.whitelist()
 def send_whatsapp_message(number, text):
     number = number.replace("+", "").replace(" ", "")
-    requests.post(
-        "https://api.evo.uat.rolaface.com/message/sendText/hrms-instance",
-        headers={"apikey": "429683C4C977415CAAFCCE10F7D57E11", "Content-Type": "application/json"},
-        json={"number": number, "text": text},
-        timeout=10
-    )
+    # requests.post(
+    #     "https://api.evo.uat.rolaface.com/message/sendText/hrms-instance",
+    #     headers={"apikey": "429683C4C977415CAAFCCE10F7D57E11", "Content-Type": "application/json"},
+    #     json={"number": number, "text": text},
+    #     timeout=10
+    # )
