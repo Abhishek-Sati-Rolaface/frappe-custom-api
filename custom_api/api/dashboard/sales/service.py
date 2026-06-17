434c2bf36479df3f1f62e281b1e6791854c920eb
CallMeNegi <rishab.negi@rolaface.com>
2026-06-17 15:11:48 +0530

    fix purchase chart response and add filter in sales

---
+++ custom_api/api/dashboard/sales/service.py
@@ -0,0 +1,29 @@
+import frappe
+
+def get_top_recent_sales_data(order_by=None):
+    
+    company = frappe.defaults.get_user_default("Company") or frappe.get_default("Company")
+
+    if not order_by:
+        order_by = "outstanding_amount desc"
+
+    recent_sales = frappe.get_all(
+        "Sales Invoice",
+        filters={
+            "docstatus": 1,
+            "company": company
+        },
+        fields=[
+            "name", 
+            "customer_name", 
+            "posting_date", 
+            "base_grand_total", 
+            "outstanding_amount", 
+            "status",
+            "currency"
+        ],
+        order_by=order_by,
+        limit_page_length=10
+    )
+
+    return recent_sales
