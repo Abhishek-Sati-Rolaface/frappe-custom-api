df9fc83586176765ef66c843c80f8289cb444576
Abhishek Sati <abhisheksati9876@gmail.com>
2026-07-03 11:57:25 +0200

    fixed filtering in sales Invoice

--- custom_api/api/selling/sales_invoice/service.py
+++ custom_api/api/selling/sales_invoice/service.py
@@ -363,6 +363,9 @@ def get_sales_invoices(filters=None, page=1, page_size=20, search=None):
     }
 
     frappe_filters = build_sales_invoice_filters(allowed_filters)
+    order_by = "creation_date desc"
+    if filters.get("sortBy"):
+        order_by = f"{filters.get('sortBy')} {filters.get('sortOrder') or 'asc'}"
 
     or_filters = []
     if search:
@@ -399,7 +402,7 @@ def get_sales_invoices(filters=None, page=1, page_size=20, search=None):
         ],
         limit_start=start,
         limit_page_length=page_size,
-        order_by="creation desc",
+        order_by=order_by,
     )
 
     total_invoices = len(
