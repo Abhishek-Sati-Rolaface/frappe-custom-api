45bf596e1b081eec17bc95e9f7a91b1bda6c98b4
Abhishek Sati <abhisheksati9876@gmail.com>
2026-07-17 16:07:53 +0200

    updated Exception name

--- custom_api/config/zra_exception.py
+++ custom_api/config/zra_exception.py
@@ -30,7 +30,7 @@ class ZRAConnectionError(Exception):
 
         frappe.db.commit()
 
-class ZRAConnectionError(Exception):
+class ZRAResponseError(Exception):
     def __init__(self, msg, doc=None, result=None):
         super().__init__(msg)
         self.doc = doc
@@ -48,14 +48,18 @@ class ZRAConnectionError(Exception):
         fresh_doc = frappe.get_doc(self.doctype, self.invoice_id)
         fresh_doc.db_set("status", new_status, update_modified=True)
 
+        result_value = self.result
+        if result_value is not None and not isinstance(result_value, str):
+            result_value = frappe.as_json(result_value)
+
         if self.doctype == "Purchase Invoice" and fresh_doc.get("custom_invoice_metadata"):
             child_row = fresh_doc.custom_invoice_metadata[0]
             if child_row.get("name"):
-                child_row.db_set("zra_response", self.result, update_modified=True)
+                child_row.db_set("zra_response", result_value, update_modified=True)
 
         elif self.doctype == "Sales Invoice" and fresh_doc.get("custom_details"):
             child_row = fresh_doc.custom_details[0]
             if child_row.get("name"):
-                child_row.db_set("zra_response", self.result, update_modified=True)
+                child_row.db_set("zra_response", result_value, update_modified=True)
 
         frappe.db.commit()
