796aa2c6a6a601c649d84fcb35dddba0435391a7
Abhishek Sati <abhishek.sati@rolaface.com>
2026-08-22 12:51:18 +0200

    removed notification schedular, from custom API

--- custom_api/custom_api/doctype/custom_notification_scheduler/custom_notification_scheduler.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, Rolaface and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Custom Notification Scheduler", {
-// 	refresh(frm) {
-
-// 	},
-// });
