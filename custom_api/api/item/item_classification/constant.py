8f7ce0690a2e011afc35393207c8bf4534018432
Abhishek Sati <abhishek.sati@rolaface.com>
2026-09-01 23:24:31 +0200

    feat(tax_handling): add ZRA item classification mapping and trigger function

---
+++ custom_api/api/item/item_classification/constant.py
@@ -0,0 +1,8 @@
+ZRA_ITEM_CLASS_MAPPING = {
+    "itemClsCd": "class_code",
+    "itemClsNm": "class_name",
+    "itemClsLvl": "class_level",
+    "taxTyCd": "tax_type_code",
+    "mjrTgYn": "major_target",
+    "useYn": "is_active",
+}
